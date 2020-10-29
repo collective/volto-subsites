aec019864575bc13a12a0819b4805d072711dbf4
Giulia Ghisini <giulia.ghisini@redturtle.it>
2020-10-29 17:00:13 +0100

    added class subsite-root to body

--- src/SubsiteLoader.jsx
+++ src/SubsiteLoader.jsx
@@ -4,17 +4,19 @@
  */
 
 import React, { useEffect } from 'react';
-
+import { useLocation } from 'react-router-dom';
 import { connect, useDispatch, useSelector } from 'react-redux';
 import { getContent, resetContent } from '@plone/volto/actions';
-import { BodyClass } from '@plone/volto/helpers';
+import { BodyClass, flattenToAppURL } from '@plone/volto/helpers';
+import cx from 'classnames';
 
-const SubsiteLoader = ({ content, pathname }) => {
+const SubsiteLoader = ({ content }) => {
   const dispatch = useDispatch();
   const subsiteEndpoint = content?.['@components']?.subsite
     ? content['@components'].subsite['@id']
     : null;
   const subsite = useSelector((state) => state.content?.subrequests?.subsite);
+  const location = useLocation();
 
   useEffect(() => {
     if (subsiteEndpoint && !subsite?.loading) {
@@ -25,10 +27,20 @@ const SubsiteLoader = ({ content, pathname }) => {
     };
   }, [content]);
 
-  return subsite?.data && !subsite?.loading ? (
+  let pathname = location.pathname.endsWith('/')
+    ? location.pathname.slice(0, -1)
+    : location.pathname;
+  return subsite && subsite.data?.['@id'] && !subsite.loading ? (
     <>
       <BodyClass
-        className={`subsite subsite-${subsite.data.subsite_css_class?.token}`}
+        className={cx(
+          'subsite',
+          `subsite-${subsite.data.subsite_css_class?.token}`,
+          {
+            'subsite-root':
+              pathname === flattenToAppURL(subsite?.data?.['@id'] ?? ''),
+          },
+        )}
       />
     </>
   ) : null;
