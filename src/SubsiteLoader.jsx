630c5998aeac8c6e7c83988f90938a62846f6aba
mamico <mauro.amico@gmail.com>
2022-08-29 21:13:51 +0200

    bbb

--- src/SubsiteLoader.jsx
+++ src/SubsiteLoader.jsx
@@ -4,7 +4,7 @@
  */
 
 import React, { useEffect } from 'react';
-import { connect, useDispatch } from 'react-redux';
+import { connect, useDispatch, useSelector } from 'react-redux';
 
 import { BodyClass } from '@plone/volto/helpers';
 import cx from 'classnames';
@@ -13,15 +13,15 @@ import { setSubsite } from './actions';
 import { useLocation } from 'react-router-dom';
 
 const SubsiteLoader = ({ content }) => {
-  const subsite = content?.['@components'].subsite;
-  // const subsite = useSelector((state) => state.subsite?.data);
+  // const subsite = content?.['@components'].subsite;
+  const subsite = useSelector((state) => state.subsite?.data);
   const location = useLocation();
   const dispatch = useDispatch();
 
   // BBB: copy state.content['@components].subsite to state.subsite for backward compatibility
   useEffect(() => {
-    dispatch(setSubsite(subsite));
-  }, [dispatch, subsite]);
+    dispatch(setSubsite(content?.['@components'].subsite));
+  }, [dispatch, content]);
 
   return subsite && subsite['@id'] ? (
     <BodyClass
